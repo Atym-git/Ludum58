503854e8cabe815990fd7b8e81fa36d8fe41c14a
Atym-git <chuvachelaad@gmail.com>
2025-10-04 15:33:41 +0300

    lil movement fix

--- Ludum58th/Assets/_Source/PlayerSystem/PlayerMovement.cs
+++ Ludum58th/Assets/_Source/PlayerSystem/PlayerMovement.cs
@@ -4,9 +4,6 @@ public class PlayerMovement
 {
     public void Move(float vertMoveF, float moveSpeed, Rigidbody2D playerRb)
     {
-        //playerRb.linearVelocity = new Vector2 (vertMoveF * moveSpeed, 0);
-        //playerRb.linearVelocityX = vertMoveF * moveSpeed;
-        Transform playerTransform = playerRb.transform;
-        playerTransform.position += new Vector3(vertMoveF * moveSpeed, 0, 0);
+        playerRb.linearVelocityX = vertMoveF * moveSpeed;
     }
 }
